e34b4436fe1bd9768169d97ed609a4b8518d4c5c
Guillaume JOOS <joos.guillaume@gmail.com>
2022-08-04 18:15:47 +0200

    correctifs affichage conditionnel

--- src/com/mycompany/hellouniverse/HelloUniverse.java
+++ src/com/mycompany/hellouniverse/HelloUniverse.java
@@ -2,17 +2,13 @@ package com.mycompany.hellouniverse;
 
 public class HelloUniverse {
     public static void main(String... args){
-        System.out.println("Hello Universe!");
-        int nombreDePlanetes=8;
-        int annee=2006;
-
-        if (annee<2006) {
-            nombreDePlanetes++;
-            System.out.printf("En %d, les planètes du système solaire étaient au nombre de : %d",annee,nombreDePlanetes);
-        }
+        String declaration = "En %d, les planètes du système solaire étaient au nombre de : %d.";
+        int nombreDePlanetes=9;
+        short annee=2003;
 
         if (annee>=2006) {
-            System.out.printf("En %d, les planètes du système solaire étaient au nombre de : %d",annee,nombreDePlanetes);
+            nombreDePlanetes--;
         }
+        System.out.printf(declaration,annee,nombreDePlanetes);
     }
 }
