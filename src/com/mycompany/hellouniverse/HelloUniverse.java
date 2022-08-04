aff5c4940142d4b2bf86867e4914b29e91dc6f55
Guillaume JOOS <joos.guillaume@gmail.com>
2022-08-04 18:02:39 +0200

    affichage conditionnel

--- src/com/mycompany/hellouniverse/HelloUniverse.java
+++ src/com/mycompany/hellouniverse/HelloUniverse.java
@@ -4,11 +4,15 @@ public class HelloUniverse {
     public static void main(String... args){
         System.out.println("Hello Universe!");
         int nombreDePlanetes=8;
-        String phraseDescription;
-        phraseDescription="Aux dernières nouvelles, le nombre total de planètes dans le système solaire est de : ";
-        System.out.println(phraseDescription + nombreDePlanetes);
-        nombreDePlanetes++;
-        phraseDescription="Il y a quelques années cependant, elles étaient au nombre de : ";
-        System.out.println(phraseDescription + nombreDePlanetes);
+        int annee=2006;
+
+        if (annee<2006) {
+            nombreDePlanetes++;
+            System.out.printf("En %d, les planètes du système solaire étaient au nombre de : %d",annee,nombreDePlanetes);
+        }
+
+        if (annee>=2006) {
+            System.out.printf("En %d, les planètes du système solaire étaient au nombre de : %d",annee,nombreDePlanetes);
+        }
     }
 }
