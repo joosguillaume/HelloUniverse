80e9356c7bd2f66f159197138d4bc378e5d79df6
Guillaume JOOS <joos.guillaume@gmail.com>
2022-08-05 08:06:40 +0200

    ajout else if

--- src/com/mycompany/hellouniverse/HelloUniverse.java
+++ src/com/mycompany/hellouniverse/HelloUniverse.java
@@ -3,14 +3,20 @@ package com.mycompany.hellouniverse;
 public class HelloUniverse {
     public static void main(String... args){
         String declaration = "En %d, les planètes du système solaire étaient au nombre de : %d.";
-        int nombreDePlanetes=9;
-        short annee=2003;
+        int nombreDePlanetes=0;
+        short annee=2021;
 
-        System.out.println(annee<2006?"a":"b");
-
-        if (annee>=2006) {
-            nombreDePlanetes--;
+        if (annee<1600 || annee>2020) {
+            System.out.printf("Le programme ne peut pas fournir de résultat pour l'année %d.",annee);
+        } else {
+            if (annee<1700) {
+                nombreDePlanetes=7;
+            } else if (annee<1800 || annee>=2006) {
+                    nombreDePlanetes=8;
+                } else {
+                        nombreDePlanetes=9;
+            }
+            System.out.printf(declaration,annee,nombreDePlanetes);
         }
-        System.out.printf(declaration,annee,nombreDePlanetes);
     }
 }
