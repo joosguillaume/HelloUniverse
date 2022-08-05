b63b6c13cb994ca66880d0c3c604b3a730299a13
Guillaume JOOS <joos.guillaume@gmail.com>
2022-08-05 08:39:08 +0200

    utilisation du switch/case

--- src/com/mycompany/hellouniverse/HelloUniverse.java
+++ src/com/mycompany/hellouniverse/HelloUniverse.java
@@ -2,21 +2,20 @@ package com.mycompany.hellouniverse;
 
 public class HelloUniverse {
     public static void main(String... args){
-        String declaration = "En %d, les planètes du système solaire étaient au nombre de : %d.";
-        int nombreDePlanetes=0;
-        short annee=2021;
+        int nombreDePlanetes=9;
 
-        if (annee<1600 || annee>2020) {
-            System.out.printf("Le programme ne peut pas fournir de résultat pour l'année %d.",annee);
-        } else {
-            if (annee<1700) {
-                nombreDePlanetes=7;
-            } else if (annee<1800 || annee>=2006) {
-                    nombreDePlanetes=8;
-                } else {
-                        nombreDePlanetes=9;
-            }
-            System.out.printf(declaration,annee,nombreDePlanetes);
+        switch (nombreDePlanetes){
+            case 7:
+                System.out.printf("On sait qu'au 16ème siècle, seules %d planètes avaient été découvertes",nombreDePlanetes);
+                break;
+            case 8:
+                System.out.printf("On sait que le nombre de planètes est passé de 7 à %d au 17ème siècle, mais il a également été réduit de 9 à %d en 2006",nombreDePlanetes,nombreDePlanetes);
+                break;
+            case 9:
+                System.out.printf("On sait que le nombre de planètes est passé de 8 à %d au 18ème siècle et ce jusqu'en 2006, où ce nombre a été réduit à 8",nombreDePlanetes);
+                break;
+            default:
+                System.out.printf("Le programme ne peut pas fournir de résultat pour %d",nombreDePlanetes);
         }
     }
 }
